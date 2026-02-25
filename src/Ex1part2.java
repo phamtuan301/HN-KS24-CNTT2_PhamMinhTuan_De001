46aa3c23d827b4cbbcddd8af8dfa78bc20947020
Phạm Minh Tuấn <tuan13nt@gmail.com>
2026-02-25 09:57:24 +0700

    commit

--- src/Ex1part2.java
+++ src/Ex1part2.java
@@ -1,22 +1,23 @@
-import java.util.Objects;
 import java.util.Scanner;
-
 public class Ex1part2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhap chuoi");
-        String n = sc.nextLine().toLowerCase();
-        int[] count = new int[26];
-        for(int i = 0; i < n.length(); ++i) {
+        System.out.println("Nhap chuoi:");
+        String n = sc.nextLine();
+        int[] count = new int[128];
+        for (int i = 0; i < n.length(); i++) {
             char c = n.charAt(i);
             if (Character.isLetter(c)) {
-                ++count[c - 97];
+                count[c]++;
             }
         }
-        for(int i = 0; i < n.length(); ++i) {
-            char c = n.charAt(i);
-            System.out.println("Ki tu " + c + " xuat hien " + count[c - 97] + " lan");
-        }
 
+        for (int i = 0; i < n.length(); i++) {
+            char c = n.charAt(i);
+
+            if (Character.isLetter(c)) {
+                System.out.println("Ki tu " + c + " xuat hien " + count[c] + " lan");
+            }
+        }
     }
 }
