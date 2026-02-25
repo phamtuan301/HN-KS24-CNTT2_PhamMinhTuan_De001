66e9e64989a413d4d6b3208f088e493137a71e2c
Phạm Minh Tuấn <tuan13nt@gmail.com>
2026-02-25 09:46:19 +0700

    commit

--- src/Ex3.java
+++ src/Ex3.java
@@ -1,24 +1,17 @@
 public class Ex3 {
     public static void main(String[] args) {
-        int[] A = new int[]{6, 10, 10, 10, 2, 5, 6, 7, 8, 3};
-        int[] B = new int[]{2, 3};
-
-        for(int i = 0; i < A.length; ++i) {
-            boolean check = false;
-            for(int k = 0; k < i; ++k) {
-                if (A[i] == A[k]) {
-                    check = true;
-                    break;
-                }
-            }
-            if (!check) {
-                for(int j = 0; j < B.length; ++j) {
-                    if(A[i] == B[j]){
-                        System.out.println(A[i] + " ");
-                    }
-                }
+        int[] A = {6, 10, 10, 10, 2, 5, 6, 7, 8, 3};
+        int[] B = {2, 3};
+        int j = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (j < B.length && A[i] == B[j]) {
+                j++;
             }
         }
-
+        if (j == B.length) {
+            System.out.println("B la mang con cua A");
+        } else {
+            System.out.println("B khong phai la mang con cua A");
+        }
     }
 }
