138dd7384c822cef14e640ede1478c32110c19fe
Phạm Minh Tuấn <tuan13nt@gmail.com>
2026-02-25 09:49:08 +0700

    commit

--- src/Ex1part1.java
+++ src/Ex1part1.java
@@ -1,19 +1,29 @@
 import java.util.Scanner;
-
 public class Ex1part1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter string");
         String s = sc.nextLine();
         int sum = 0;
-        String[] arr = s.split(" ");
-        for(int i = 0; i < arr.length; ++i) {
-            for(int j = 0; j < arr[i].length(); ++j) {
-                if(!Character.isLetter(arr[i].charAt(j)))  {
-                    sum += Character.getNumericValue(arr[i].charAt(j));
+        int currentNumber = 0;
+        boolean isBuildingNumber = false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                currentNumber = currentNumber * 10 + (c - '0');
+                isBuildingNumber = true;
+            } else {
+                if (isBuildingNumber) {
+                    sum += currentNumber;
+                    currentNumber = 0;
+                    isBuildingNumber = false;
                 }
             }
         }
+        if (isBuildingNumber) {
+            sum += currentNumber;
+        }
+
         System.out.println(sum);
     }
 }
